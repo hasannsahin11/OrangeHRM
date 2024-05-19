3fed75595d60deb4adc9bf7d64988f6c071893b3
hasansahin11 <qa.hasansahin@gmail.com>
2024-05-18 22:17:07 -0500

    Emergency Contacts Feature steps defined.

--- src/test/java/pages/EmergencyContacts.java
+++ src/test/java/pages/EmergencyContacts.java
@@ -8,17 +8,24 @@ import utils.MyMethods;
 
 public class EmergencyContacts extends MyMethods {
 
-    public EmergencyContacts(){
-        PageFactory.initElements(DriverClass.getDriver(),this);
+    public EmergencyContacts() {
+        PageFactory.initElements(DriverClass.getDriver(), this);
     }
 
 
     @FindBy(css = "a[href='/web/index.php/pim/viewEmergencyContacts/empNumber/22']")
     private WebElement emergencyContactsBtn;
 
+    @FindBy(xpath = "(//button[@type='button'])[3]")
+    private WebElement addBtn;
 
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input') and contains(@class,'oxd-input--active')])[2]")
+    private WebElement nameField;
 
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input') and contains(@class,'oxd-input--active')])[3]")
+    private WebElement relationshipField;
 
-
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input') and contains(@class,'oxd-input--active')])[5]")
+    private WebElement mobileNumField;
 
 }
--- src/test/java/stepDefinitions/_07_EmergencyContactsSteps.java
+++ src/test/java/stepDefinitions/_07_EmergencyContactsSteps.java
@@ -1,36 +1,27 @@
 package stepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
 
 public class _07_EmergencyContactsSteps {
     @Given("I navigate to Emergency Contacts section")
     public void i_navigate_to_emergency_contacts_section() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("I click on the Add button")
     public void i_click_on_the_add_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
-    @When("I fill out the following fields with the corresponding data:")
-    public void i_fill_out_the_following_fields_with_the_corresponding_data(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    @When("I fill out the following mandatory fields with the corresponding data:")
+    public void i_fill_out_the_following_mandatory_fields_with_the_corresponding_data(DataTable dataTable) {
+
     }
 
     @Then("The new Emergency Contact should be successfully saved and displayed")
     public void the_new_emergency_contact_should_be_successfully_saved_and_displayed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
