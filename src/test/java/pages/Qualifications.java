ac7302c0635d2d6d435650838259899985b81738
hasansahin11 <qa.hasansahin@gmail.com>
2024-06-03 13:07:41 -0500

    Locators added for the scenario: Verify Qualifications Headers

--- src/test/java/pages/Qualifications.java
+++ src/test/java/pages/Qualifications.java
@@ -1,4 +1,33 @@
 package pages;
 
-public class Qualifications {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.DriverClass;
+import utils.MyMethods;
+
+public class Qualifications extends MyMethods {
+
+    public Qualifications(){
+        PageFactory.initElements(DriverClass.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//h6[contains(.,'Work Experience')]")
+    private WebElement workExperience;
+
+@FindBy(xpath = "//h6[contains(.,'Education')]")
+    private WebElement education;
+
+@FindBy(xpath = "//h6[contains(.,'Skills')]")
+    private WebElement skills;
+
+    @FindBy(xpath = "//h6[contains(.,'Languages')]")
+    private WebElement languages;
+
+    @FindBy(xpath = "//h6[contains(.,'License')]")
+    private WebElement license;
+
+    @FindBy(xpath = "//h6[contains(.,'Attachments')]")
+    private WebElement attachments;
+
 }
