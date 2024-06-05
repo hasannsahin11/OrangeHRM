0c15e44996222be87e187416f6eef895a5f87bee
hasansahin11 <qa.hasansahin@gmail.com>
2024-06-04 23:21:59 -0500

    Steps defined: When I scroll down to the bottom of the page

--- src/test/java/stepDefinitions/MI_06_QualificationsSteps.java
+++ src/test/java/stepDefinitions/MI_06_QualificationsSteps.java
@@ -6,9 +6,13 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import pages.Qualifications;
 import utils.DriverClass;
+
+import java.util.List;
 
 public class MI_06_QualificationsSteps {
 
@@ -22,59 +26,57 @@ public class MI_06_QualificationsSteps {
 
     @When("I scroll down to the bottom of the page")
     public void i_scroll_down_to_the_bottom_of_the_page() throws InterruptedException {
+
+        q.wait.until(ExpectedConditions.urlContains("viewQualifications"));
+        q.wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".employee-image")));
         JavascriptExecutor js = (JavascriptExecutor) DriverClass.getDriver();
 
 
         q.wait.until(ExpectedConditions.visibilityOf(q.getWorkExperience()));
-
         if (q.getWorkExperience().isDisplayed()) {
-            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
+            js.executeScript("window.scrollBy(0, arguments[0]);", 150);
         }
 
         q.wait.until(ExpectedConditions.visibilityOf(q.getEducation()));
         if (q.getEducation().isDisplayed()) {
-            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
+            js.executeScript("window.scrollBy(0, arguments[0]);", 150);
         }
 
         q.wait.until(ExpectedConditions.visibilityOf(q.getSkills()));
         if (q.getSkills().isDisplayed()) {
-            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
+            js.executeScript("window.scrollBy(0, arguments[0]);", 150);
         }
 
         q.wait.until(ExpectedConditions.visibilityOf(q.getLanguages()));
         if (q.getLanguages().isDisplayed()) {
-            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
+            js.executeScript("window.scrollBy(0, arguments[0]);", 150);
 
         }
 
         q.wait.until(ExpectedConditions.visibilityOf(q.getLicense()));
         if (q.getLicense().isDisplayed()) {
-            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
+            js.executeScript("window.scrollBy(0, arguments[0]);", 150);
         }
 
         q.wait.until(ExpectedConditions.visibilityOf(q.getAttachments()));
         if (q.getAttachments().isDisplayed()) {
             js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
         }
-
-//            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
-//            Thread.sleep(1500);
-//            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
-//            Thread.sleep(1500);
-//            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
-//            Thread.sleep(3000);
-//            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
-//            Thread.sleep(3000);
-//            js.executeScript("window.scrollBy(0, arguments[0]);", 200);
-        }
-
-        @Then("I should see the following Qualification headers:")
-        public void i_should_see_the_following_qualification_headers (DataTable dataTable){
-
-        }
-
-        public void scroll () {
-
-        }
-
     }
+
+    @Then("I should see the following Qualification headers:")
+    public void i_should_see_the_following_qualification_headers(DataTable dataTable) {
+//        List<String> headers = dataTable.asList();
+//
+//        for (String qHeaders : headers) {
+//            Assert.assertTrue(getHeader(qHeaders).isDisplayed());
+//            System.out.println("Displayed Qualifications Header: " + getHeader(qHeaders).getText());
+//        }
+    }
+
+//    public WebElement getHeader(String headerName) {
+//        WebElement header = DriverClass.getDriver().findElement(By.xpath("//h6[contains(.,'" + headerName + "')]"));
+//        return header;
+//    }
+
+}
