49712f428010b1d3594e9018a2ab84b05fe7fa6d
hasansahin11 <qa.hasansahin@gmail.com>
2024-06-20 00:07:05 +0300

    Steps defined

--- src/test/java/stepDefinitions/MI_06_QualificationsSteps.java
+++ src/test/java/stepDefinitions/MI_06_QualificationsSteps.java
@@ -188,4 +188,12 @@ public class MI_06_QualificationsSteps {
         DriverClass.quitDriver();
 
     }
+
+    @When("I click on the Delete icon of the created Work Experience")
+    public void iClickOnTheDeleteIconOfTheCreatedWorkExperience() {
+    }
+
+    @Then("The Work Experience should be successfully deleted and removed from the list")
+    public void theWorkExperienceShouldBeSuccessfullyDeletedAndRemovedFromTheList() {
+    }
 }
