9bacb274722c0cbc1b77ea4795cab00303022422
hasansahin11 <qa.hasansahin@gmail.com>
2024-06-23 16:11:13 +0300

    Steps updated.

--- src/test/java/stepDefinitions/MI_06_QualificationsSteps.java
+++ src/test/java/stepDefinitions/MI_06_QualificationsSteps.java
@@ -196,5 +196,6 @@ public class MI_06_QualificationsSteps {
 
     @Then("The Work Experience should be successfully deleted and removed from the list")
     public void theWorkExperienceShouldBeSuccessfullyDeletedAndRemovedFromTheList() {
+
     }
 }
