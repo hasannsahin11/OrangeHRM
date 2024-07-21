632604c618e4f7862467b9a4f0ae190e76fc80e0
hasansahin11 <qa.hasansahin@gmail.com>
2024-07-21 13:46:56 -0500

    DriverClass update.

--- src/test/java/utils/DriverClass.java
+++ src/test/java/utils/DriverClass.java
@@ -11,28 +11,28 @@ import java.time.Duration;
 public class DriverClass {
     public static WebDriver driver;
 
-    @BeforeClass
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-        }
-        return driver;
-    }
-
-    @AfterClass
-    public static void quitDriver() {
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driver.quit();
-        driver = null;
-    }
+//    @BeforeClass
+//    public static WebDriver getDriver() {
+//        if (driver == null) {
+//            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+//            driver = new ChromeDriver();
+//            driver.manage().window().maximize();
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+//        }
+//        return driver;
+//    }
+//
+//    @AfterClass
+//    public static void quitDriver() {
+//        try {
+//            Thread.sleep(4000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        driver.quit();
+//        driver = null;
+//    }
 
     public static void setThreadDriverName(String browserName){
 
