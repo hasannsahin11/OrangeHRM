3b3fa2ddd4ddd4ce548775f882933edb3f4312f6
hasansahin11 <qa.hasansahin@gmail.com>
2024-07-20 23:49:56 -0500

    Cucumber Options added for the parallel runner.

--- src/test/java/runners/_04_ParallelTestRunner.java
+++ src/test/java/runners/_04_ParallelTestRunner.java
@@ -1,4 +1,15 @@
 package runners;
 
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@Smoke or @@Regression",
+        features = {"C:\\Users\\hasan\\IdeaProjects\\OrangeHRM\\src\\test\\java\\features"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+
+)
 public class _04_ParallelTestRunner {
+
+
 }
