476586f75d37da284dc3f35b2c85bfb3dacc40d6
hasansahin11 <qa.hasansahin@gmail.com>
2024-07-28 23:56:32 -0500

    Pretty report plugin has been added for RegressionRunner

--- src/test/java/runners/_02_RegressionRunner.java
+++ src/test/java/runners/_02_RegressionRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@Regression",
         features = {"C:\\Users\\hasan\\IdeaProjects\\OrangeHRM\\src\\test\\java\\features"},
-        glue = {"stepDefinitions"}
+        glue = {"stepDefinitions"},
+        plugin = {"html:target/site/cucumber-pretty.html"}
 )
 public class _02_RegressionRunner extends AbstractTestNGCucumberTests {
 }
