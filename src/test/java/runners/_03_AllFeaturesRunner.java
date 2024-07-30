64e97b04eb2a7c24c471fc968080de1eeda77a4b
hasansahin11 <qa.hasansahin@gmail.com>
2024-07-30 14:28:38 -0500

    Pretty report plugin has been added for AllFeatures Runner

--- src/test/java/runners/_03_AllFeaturesRunner.java
+++ src/test/java/runners/_03_AllFeaturesRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"C:\\Users\\hasan\\IdeaProjects\\OrangeHRM\\src\\test\\java\\features"},
         glue = {"stepDefinitions"},
-        dryRun = true
+        dryRun = true,
+        plugin = {"html:target/site/cucumber-pretty.html"}
 )
 public class _03_AllFeaturesRunner extends AbstractTestNGCucumberTests {
 }
