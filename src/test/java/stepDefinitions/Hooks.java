b2ba2378958e763b5848d4cfa96add7e572476be
hasansahin11 <qa.hasansahin@gmail.com>
2024-08-03 14:35:50 -0500

    Screenshot config added to Hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,6 +2,9 @@ package stepDefinitions;
 
 import io.cucumber.java.*;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utils.DriverClass;
 
 public class Hooks {
     @Before // This method runs before every scenario
@@ -11,6 +14,12 @@ public class Hooks {
 
     @After // This one runs after all the scenarios
     public void afterScenario(Scenario scenario) {
+
+        if(scenario.isFailed()){
+            final byte[] byteImage = ((TakesScreenshot) DriverClass.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(byteImage, "image/png", "screenShot name");
+        }
+
         System.out.println("Scenario has ended!");
     }
 }
