a788e2b59210ad0932d787ba82f2aaceef69e4d7
hasansahin11 <qa.hasansahin@gmail.com>
2024-08-05 14:35:41 -0500

    Runner class updated for extent.

--- src/test/java/runners/_01_SmokeTestRunner.java
+++ src/test/java/runners/_01_SmokeTestRunner.java
@@ -1,15 +1,28 @@
 package runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
         tags = "@Smoke",
         features = {"C:\\Users\\hasan\\IdeaProjects\\OrangeHRM\\src\\test\\java\\features"},
         glue = {"stepDefinitions"},
-        plugin = {"html:target/site/cucumber-pretty.html"}
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 
 public class _01_SmokeTestRunner extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name", "H.Sahin");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Additional line", "Additional Info");
+    }
 
 }
