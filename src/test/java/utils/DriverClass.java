fb9a17b161626ac950f026b3a08c583847e78c59
HS_DT <qa.hasansahin@gmail.com>
2024-09-16 19:29:44 -0500

    Driver class updated to suite Jenkins screen resolution

--- src/test/java/utils/DriverClass.java
+++ src/test/java/utils/DriverClass.java
@@ -36,7 +36,8 @@ public class DriverClass {
                 default:
                     ChromeOptions options = new ChromeOptions();
                     if (!runningFromIntelliJ()) {
-                        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+//                        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+                        options.addArguments("window-size=1920,1080");
                     }
                     threadDriver.set(new ChromeDriver());
             }
