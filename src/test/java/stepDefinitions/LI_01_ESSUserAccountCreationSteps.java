d60123865b9c63fc24cf9c594d20713761fb1cc5
HS_DT <qa.hasansahin@gmail.com>
2024-09-17 20:45:41 -0500

    Selenium dependency update & i_click_the_add_button() step update.

--- src/test/java/stepDefinitions/LI_01_ESSUserAccountCreationSteps.java
+++ src/test/java/stepDefinitions/LI_01_ESSUserAccountCreationSteps.java
@@ -59,24 +59,9 @@ public class LI_01_ESSUserAccountCreationSteps {
     @And("I click the Add button")
     public void i_click_the_add_button() {
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
-        WebElement element = DriverClass.getDriver().findElement(By.xpath("(//button[contains(@class, 'oxd-button') and contains(@class, 'oxd-button--medium') and contains(@class, 'oxd-button--secondary')])[2]"));
-        ((JavascriptExecutor) DriverClass.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        element.click();
-
-//        ap.getAddBtn().click();
+        ap.wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[contains(@class, 'oxd-button') and contains(@class, 'oxd-button--medium') and contains(@class, 'oxd-button--secondary')])[2]")));
+        ap.getAddBtn().click();
 
         System.out.println("Clicked on the Add button");
 
